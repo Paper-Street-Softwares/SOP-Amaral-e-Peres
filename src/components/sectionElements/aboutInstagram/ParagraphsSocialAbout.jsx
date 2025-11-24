de0eeca1bf454968491c2f9bd2223c76697fa878
Gabriel Souza <gabrielsouza65115@gmail.com>
2025-11-24 17:24:08 -0300

    fix: Copy alterado e secao criada

--- src/components/sectionElements/aboutInstagram/ParagraphsSocialAbout.jsx
+++ src/components/sectionElements/aboutInstagram/ParagraphsSocialAbout.jsx
@@ -4,16 +4,16 @@ import content from "../../../content/content";
 import MotionDivDownToUp from "../../animation/MotionDivDownToUp";
 import { useTranslation } from "react-i18next";
 
-function ParagraphsAboutSocial() {
+function ParagraphsSocialAbout() {
   const { t } = useTranslation();
 
   return (
     <MotionDivDownToUp>
-      <Paragraphs className="text-black text-opacity-80 mb-[48px] text-centjustifyer">
-        {t("about.aboutWpp.paragraph")}{" "}
+      <Paragraphs className="text-black text-opacity-80 mb-[48px]">
+        {t("about.aboutSocial.paragraph")}{" "}
       </Paragraphs>
     </MotionDivDownToUp>
   );
 }
 
-export default ParagraphsAboutSocial;
+export default ParagraphsSocialAbout;
